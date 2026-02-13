b08c55f25836fd0551dbe1137274d3ac734d26a8
kamleshsaran371 <kamleshsaran371@gmail.com>
2026-02-13 18:10:44 +0530

    Add files via upload

---
+++ star/modules/helpers/read.py
@@ -0,0 +1,49 @@
+from config import OWNER_USERNAME, SUPPORT_GRP, UPDATE_CHNL
+from Star import StarX
+
+START = f"""
+**๏ ʜᴇʏ, ɪ ᴀᴍ [{StarX.name}](t.me/{StarX.username})**
+**➻ ᴀɴ ᴀɪ ʙᴀsᴇᴅ ᴄʜᴀᴛʙᴏᴛ**
+**──────────────**
+**➻ ᴜsᴀɢᴇ /chatbot [ᴏɴ/ᴏғғ]**
+<b>||๏ ʜɪᴛ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ғᴏʀ ʜᴇʟᴘ.||</b>
+"""
+
+HELP_READ = f"""
+<u>**ᴄᴏᴍᴍᴀɴᴅs ғᴏʀ {StarX.name}**</u>
+<u>**ᴀʀᴇ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ!**</u>
+**ᴀʟʟ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ:/**
+**──────────────**
+<b>||©️ @{OWNER_USERNAME}||</b>
+"""
+
+TOOLS_DATA_READ = f"""
+<u>**ᴛᴏᴏʟs ғᴏʀ {StarX.name} ᴀʀᴇ:**</u>
+**➻ ᴄʟɪᴄᴋ ᴏɴ sᴏᴜʀᴄᴇ ʙᴜᴛᴛᴏɴ ғᴏʀ sᴏᴜʀᴄᴇ ᴄʜᴀɴɴᴇʟ!**
+**──────────────**
+**➻ ᴜsᴇ /ping ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴛʜᴇ ᴘɪɴɢ ᴏғ {StarX.name}**
+**──────────────**
+**➻ ᴜsᴇ /id ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴜsᴇʀ ɪᴅ, ᴄʜᴀᴛ ɪᴅ ᴀɴᴅ ᴍᴇssᴀɢᴇ ɪᴅ ᴀʟʟ ɪɴ ᴀ sɪɴɢʟᴇ ᴍᴇssᴀɢᴇ.**
+**──────────────**
+<b>||©️ @{OWNER_USERNAME}||</b>
+"""
+
+CHATBOT_READ = f"""
+<u>**ᴄᴏᴍᴍᴀɴᴅs ғᴏʀ {StarX.name}**</u>
+**➻ ᴜsᴇ /chatbot ᴛᴏ ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ ᴛʜᴇ ᴄʜᴀᴛʙᴏᴛ.**
+**๏ ɴᴏᴛᴇ ➻ ᴛʜᴇ ᴀʙᴏᴠᴇ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀ ᴄʜᴀᴛʙᴏᴛ ᴡᴏʀᴋ ɪɴ ɢʀᴏᴜᴘ ᴏɴʟʏ!!**
+**───────────────**
+<b>||©️ @{OWNER_USERNAME}||</b>
+"""
+
+SOURCE_READ = f"**ʜᴇʏ, ᴛʜᴇ sᴏᴜʀᴄᴇ ᴄʜᴀɴɴᴇʟ ᴏғ [{StarX.name}](https://t.me/{StarX.username}) ɪs ɢɪᴠᴇɴ ʙᴇʟᴏᴡ.**\n**──────────────────**\n**ʜᴇʀᴇ ɪs ᴛʜᴇ [sᴏᴜʀᴄᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/{UPDATE_CHNL})**\n**──────────────────**\n**ɪғ ʏᴏᴜ ғᴀᴄᴇ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ ᴛʜᴇɴ ᴄᴏɴᴛᴀᴄᴛ ᴀᴛ [sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ](https://t.me/{SUPPORT_GRP}).\n<b>||©️ {OWNER_USERNAME}||</b>"
+ADMIN_READ = f"sᴏᴏɴ"
+
+ABOUT_READ = f"""
+**➻ [{StarX.name}](https://t.me/{StarX.username}) ɪs ᴀɴ ᴀɪ ʙᴀsᴇᴅ ᴄʜᴀᴛ-ʙᴏᴛ.**
+**➻ [{StarX.name}](https://t.me/{StarX.username}) ʀᴇᴘʟɪᴇs ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴛᴏ ᴀ ᴜsᴇʀ.**
+**➻ ʜᴇʟᴘs ʏᴏᴜ ɪɴ ᴀᴄᴛɪᴠᴀᴛɪɴɢ ʏᴏᴜʀ ɢʀᴏᴜᴘs.**
+**➻ ᴡʀɪᴛᴛᴇɴ ɪɴ [ᴘʏᴛʜᴏɴ](https://www.python.org) ᴡɪᴛʜ [ᴍᴏɴɢᴏ-ᴅʙ](https://www.mongodb.com) ᴀs ᴀ ᴅᴀᴛᴀʙᴀsᴇ**
+**──────────────**
+**➻ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ғᴏʀ ɢᴇᴛᴛɪɴɢ ʙᴀsɪᴄ ʜᴇʟᴩ ᴀɴᴅ ɪɴғᴏ ᴀʙᴏᴜᴛ [{StarX.name}](https://t.me/{StarX.username})**
+"""
