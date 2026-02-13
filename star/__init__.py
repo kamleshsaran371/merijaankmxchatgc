1fabadce3ac8efe626f721a72b72efd9e96191b6
kamleshsaran371 <kamleshsaran371@gmail.com>
2026-02-13 18:05:42 +0530

    Add files via upload

--- star/__init__.py
+++ star/__init__.py
@@ -1,1 +1,58 @@
+import asyncio
+import logging 
+import time
 
+# Python 3.14 no longer creates a default event loop automatically.
+# Pyrogram's sync helpers still call asyncio.get_event_loop() at import time,
+# so we bootstrap one before importing pyrogram-dependent packages.
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
+
+from Abg import patch
+
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+
+import config
+
+logging.basicConfig(
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+LOGGER = logging.getLogger(__name__)
+boot = time.time()
+mongo = MongoCli(config.MONGO_URL)
+db = mongo.Anonymous
+OWNER = config.OWNER_ID
+
+class StarX(Client):
+    def __init__(self):
+        super().__init__(
+            name="StarX",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            lang_code="en",
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.DEFAULT,
+        )
+
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
+
+    async def stop(self):
+        await super().stop()
+
+
+StarX = StarX()
